6884e41e52b9e42fb9403a0bc1b7f6897031547b
Im-adom <graceamaningkwarteng@gmail.com>
2023-05-29 05:46:15 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -13,6 +13,15 @@ int _printf(const char *format, ...)
 	{"c", print_char},
 	{"s", print_string},
 	{"%", print_percent},
+	{"d", print_integer},
+	{"i", print_integer},
+	{"b", print_binary},
+	{"u", unsigned_integer},
+	{"o", print_octal},
+	{"x", print_hex},
+	{"X", print_heX},
+	{"r", print_reversed},
+	{"R", rot13},
 	{NULL, NULL}
 	};
 
